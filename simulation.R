7a8f4a440c21691e8b2c4c288c84e097cc592355
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-04-11 17:47:42 +0200

    before removing functions

--- simulation.R
+++ simulation.R
@@ -5,34 +5,44 @@ k = .5
 beta = .3
 
 ss = 20
-ss_ratio <- c(1.01, 1.02, 1.05, 1.10, 1.15, 1.25, 1.35, 1.45, 
-              1.65, 1.85, 2.05, 2.25, 2.65, 3.05, 3.45, 3.85)
-delay <- c(1, 3, 5, 8, 14, 30, 60, 120)
+ss_ratios <- c(1.01, 1.02, 1.05, 1.10, 1.15, 1.25, 1.35, 1.45, 
+               1.65, 1.85, 2.05, 2.25, 2.65, 3.05, 3.45, 3.85)
+delays <- c(1, 3, 5, 8, 14, 30, 60, 120)
 
 
-compute_sv <- function(ss, ss_ratio, k, delay){
-  a = ss*sample(ss_ratio,1)
+compute_sv <- function(ss, ss_ratios, k, delays){
+  a = ss*sample(ss_ratios,1)
   k = k
-  D = sample(delay,1)
+  D = sample(delays,1)
   sv = a/(1+k*D)
-  return(sv)
+  return(list(a,k,D,sv))
 }
 
-softmax <- function(sv, ss, beta){
-  p_ll <- exp(sv/beta)/(exp(sv/beta)+exp(ss/beta))
-  return(p_ll)
+
+get_choice <- function(sv, ss, beta){
+  p_ll <- exp(sv*beta)/(exp(sv*beta)+exp(ss*beta))
+  random_number <- runif(1)
+  choice <- ifelse(random_number > p_ll, 'ss', 'll')
+  return(list(beta,choice))
 }
 
-sv <- compute_sv(ss, ss_ratio, k, delay)
-p_ll <- softmax(sv, ss, beta)
 
-
-
-
-'for (i in n){
-  
-  for (j in num_trials){
-    sv_ll <- compute_sv()
-    p_ll <- softmax()
+simulate <- function(n, num_trials, ss, ss_ratios, k, delays, beta){
+  df <- data.frame(participant=integer(),
+                   trial_number=integer(),
+                   ss=integer(),
+                   ll=integer(),
+                   k=integer(),
+                   delay=integer(),
+                   beta=integer(),
+                   choice=character())
+  for (i in n){
+    for (j in num_trials){
+      a, k, D, sv <- compute_sv(ss, ss_ratios, k, delays)
+      beta, choice <- get_choice(sv, ss, beta)
+      df <- rbind(df, list(i, j, ss, sv, k))
+    }
   }
-}'
+  return(df)
+}
+
