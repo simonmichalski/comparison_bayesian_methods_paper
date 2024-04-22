9e65fea1cf43ad77ae257386400c27761ad3a418
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-04-22 14:04:28 +0200

    change functions

--- simulation.R
+++ simulation.R
@@ -1,8 +1,9 @@
 
 num_trials = 128
 n = 40
-k = .5
-beta = .3
+
+k_dist = c(1,2)
+beta_dist = c(1,2)
 
 ss = 20
 ss_ratios <- c(1.01, 1.02, 1.05, 1.10, 1.15, 1.25, 1.35, 1.45, 
@@ -10,48 +11,38 @@ ss_ratios <- c(1.01, 1.02, 1.05, 1.10, 1.15, 1.25, 1.35, 1.45,
 delays <- c(1, 3, 5, 8, 14, 30, 60, 120)
 
 
-compute_sv <- function(ss, ss_ratios, k, delays){
-  a = ss*sample(ss_ratios,1)
-  k = k
-  D = sample(delays,1)
+get_choice <- function(a, k, D, ss, beta){
   sv = a/(1+k*D)
-  return(list(a,k,D,sv))
-}
-
-
-get_choice <- function(sv, ss, beta){
   p_ll <- exp(sv*beta)/(exp(sv*beta)+exp(ss*beta))
   random_number <- runif(1)
   choice <- ifelse(random_number > p_ll, 'ss', 'll')
-  return(list(beta,choice))
+  return(choice)
 }
 
 
-simulate <- function(n, num_trials, ss, ss_ratios, k, delays, beta){
+simulate <- function(n, k_dist, sd, beta_dist, num_trials, ss, ss_ratios, delays){
   df <- data.frame()
   for (i in 1:n){
+    k = sample(k_dist,1)
+    k_cond = k + rnorm(1, mean = 0, sd = sd)
+    beta = sample(beta_dist,1)
+    
     for (j in 1:num_trials){
       a = ss*sample(ss_ratios,1)
-      k = k
       D = sample(delays,1)
-      sv = a/(1+k*D)
-      beta = beta
-      p_ll <- exp(sv*beta)/(exp(sv*beta)+exp(ss*beta))
-      random_number <- runif(1)
-      choice <- ifelse(random_number > p_ll, 'ss', 'll')
-      df <- rbind(df, c(i, j, ss, a, k, D, sv, beta, p_ll, random_number, choice))
+      
+      choice <- get_choice(a, k, D, ss, beta)
+      df <- rbind(df, c(i, 0, j, k, beta, choice))
+      
+      choice_cond <- get_choice(a, k_cond, D, ss, beta)
+      df <- rbind(df, c(i, 1, j, k_cond, beta, choice_cond))
     }
   }
-  colnames(df) <- c('participant', 'trial_number', 'ss', 'll', 'k', 'delay', 
-                    'sv', 'beta', 'p_ll', 'random_number', 'choice')
+  colnames(df) <- c('subject', 'condition', 'trial', 'k', 'beta', 'choice')
   return(df)
 }
 
 
-test <- simulate(n, num_trials, ss, ss_ratios, k, delays, beta)
-
-
-# Sample from normal distribution
-rnorm(1, mean = 0, sd = 1)
+sd_1 <- simulate(n, k, 1, beta, num_trials, ss, ss_ratios, delays)
 
 
