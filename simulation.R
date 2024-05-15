27516d72ae050a4d992015d167cd23bec6d547b3
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-05-15 16:51:18 +0200

    change = to ->

--- simulation.R
+++ simulation.R
@@ -1,18 +1,18 @@
 
-n_subj = 40
-num_trials = 128
+n_subj <- 40
+num_trials <- 128
 
-k_dist = c(0.1,0.2)
-beta_dist = c(1,2)
+k_dist <- c(0.1,0.2)
+beta_dist <- c(1,2)
 
-ss = 20
+ss <- 20
 ss_ratios <- c(1.01, 1.02, 1.05, 1.10, 1.15, 1.25, 1.35, 1.45, 
                1.65, 1.85, 2.05, 2.25, 2.65, 3.05, 3.45, 3.85)
 delays <- c(1, 3, 5, 8, 14, 30, 60, 120)
 
 
 get_choice <- function(a, k, delay, ss, beta){
-  sv = a/(1+k*delay)
+  sv <- a/(1+k*delay)
   p_ll <- exp(sv*beta)/(exp(sv*beta)+exp(ss*beta))
   random_number <- runif(1)
   # ss=0; ll=1
@@ -24,13 +24,13 @@ get_choice <- function(a, k, delay, ss, beta){
 simulate <- function(n_subj, k_dist, sd, beta_dist, num_trials, ss, ss_ratios, delays){
   df <- data.frame()
   for (i in 1:n_subj){
-    k = sample(k_dist,1)
-    k_cond = k + rnorm(1, mean = 0, sd = sd)
-    beta = sample(beta_dist,1)
+    k <- sample(k_dist,1)
+    k_cond <- k + rnorm(1, mean = 0, sd = sd)
+    beta <- sample(beta_dist,1)
     
     for (j in 1:num_trials){
-      a = ss*sample(ss_ratios,1)
-      delay = sample(delays,1)
+      a <- ss*sample(ss_ratios,1)
+      delay <- sample(delays,1)
       
       choice <- get_choice(a, k, delay, ss, beta)
       df <- rbind(df, c(i, 0, j, ss, a, delay, choice))
