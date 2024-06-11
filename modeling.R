60f895e9d9fbfe00c98fc7855422445092189884
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-06-11 15:27:56 +0200

    onedrive problem

--- modeling.R
+++ modeling.R
@@ -4,7 +4,7 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
 
-data <- data_sd_0_1
+data <- sd_2$data
 
 # Create 3d arrays; condition index 1:2
 condition_values <- data$condition
@@ -16,26 +16,26 @@ choice_array <- xtabs(choice ~ subject + condition + trial, data = data)
 
 
 stan_data <- list(
-  n_subj <- 40,
-  num_cond <- 2,
-  num_trials <- 128,
-  condition <- condition_array,
-  ss <- ss_array,
-  ll <- ll_array,
-  delay <- delay_array,
-  choice <- choice_array
+  n_subj = 40,
+  num_cond = 2,
+  num_trials = 128,
+  condition = condition_array,
+  ss = ss_array,
+  ll = ll_array,
+  delay = delay_array,
+  choice = choice_array
 )
 
+model <- stan_model('discounting_model.stan')
 
-fit <- stan(
-  file = 'discounting_model.stan',
+fit <- sampling(
+  model,
   data = stan_data,
-  chains = 1,
+  chains = 2,
   iter = 2000,
   warmup = 1000,
-  thin = 1,
-  init = 'random',
-  algorithm = "HMC"
+  thin = 1
 )
 
-saveRDS(fit, "out/models/discounting_model.rds")
+
+saveRDS(fit, "out/models/test_model_sd_2.rds")
