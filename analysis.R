6c2c748bb7b37e09babd5ec663f7924c3c69ee86
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-06-11 15:25:55 +0200

    add file

---
+++ analysis.R
@@ -0,0 +1,46 @@
+library("bayesplot")
+library("rstan")
+
+fit <- readRDS("out/models/test_model_sd_2.rds")
+sim <- readRDS("out/models/test_sd_2.rds")
+sim_params <- sim$params
+
+
+summary_list <- summary(fit)
+summary_all_chains <- summary_list$summary
+
+
+correlate_model_simulation <- function(summary_all_chains, sim_params){
+  log_k_model <- vector()
+  log_k_cond_model <- vector()
+  beta_model <- vector()
+  
+  for (i in 1:40){
+    log_k_model[i] <- summary_all_chains[paste0("log_k[",i,"]"), "mean"]
+    log_k_cond_model[i] <- summary_all_chains[paste0("log_k_cond[",i,"]"), "mean"]
+    beta_model[i] <- summary_all_chains[paste0("beta[",i,"]"), "mean"]
+  }
+  
+  log_k_sim <- sim_params$log_k
+  log_k_cond_sim <- sim_params$log_k_cond
+  beta_sim <- sim_params$beta
+  
+  print(paste("r_log(k):", cor(log_k_model, log_k_sim)))
+  print(paste("r_log(k)_cond:", cor(log_k_cond_model, log_k_cond_sim)))
+  print(paste("r_beta:", cor(beta_model, beta_sim)))
+}
+
+
+correlate_model_simulation(summary_all_chains, sim_params)
+
+
+# Plot
+posterior <- as.matrix(fit)
+mcmc_areas(posterior, pars = c('log_k_cond[1]', 'log_k_cond[2]', 'log_k_cond[3]'), prob = 0.95)
+mcmc_areas(posterior, pars = c('mu_log_k'), prob = 0.95)
+mcmc_areas(posterior, pars = c('mu_log_k_cond'), prob = 0.95)
+mcmc_areas(posterior, pars = c('mu_beta'), prob = 0.95)
+
+
+
+
