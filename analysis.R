73300ee75f909353d52b96444017cc6c64e47e92
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-07-11 16:18:18 +0200

    remove sim fit corr

--- analysis.R
+++ analysis.R
@@ -1,37 +1,6 @@
 library("bayesplot")
 library("rstan")
 
-fit <- readRDS("out/test2_sd_0_51/model_prior_sd_0_2.rds")
-sim_params <- readRDS("out/test2_sd_0_51/params.rds")
-
-
-summary_list <- summary(fit)
-summary_all_chains <- summary_list$summary
-
-
-correlate_model_simulation <- function(summary_all_chains, sim_params){
-  log_k_model <- vector()
-  s_log_k_model <- vector()
-  beta_model <- vector()
-  
-  for (i in 1:40){
-    log_k_model[i] <- summary_all_chains[paste0("log_k[",i,"]"), "mean"]
-    s_log_k_model[i] <- summary_all_chains[paste0("s_log_k[",i,"]"), "mean"]
-    beta_model[i] <- summary_all_chains[paste0("beta[",i,"]"), "mean"]
-  }
-  
-  log_k_sim <- sim_params$log_k
-  s_log_k_sim <- sim_params$s_log_k
-  beta_sim <- sim_params$beta
-  
-  print(paste("r_log(k):", cor(log_k_model, log_k_sim)))
-  print(paste("r_s_log(k):", cor(s_log_k_model, s_log_k_sim)))
-  print(paste("r_beta:", cor(beta_model, beta_sim)))
-}
-
-
-correlate_model_simulation(summary_all_chains, sim_params)
-
 
 #R-hat
 rhat_mu_S_log_k <- summary_list$summary['mu_s_log_k', 'Rhat']
