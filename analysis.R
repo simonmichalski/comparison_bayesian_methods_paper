dcd0b8a76cee868b83e05bf9bc877b42e55c65db
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-07-25 11:51:53 +0200

    add comments

--- analysis.R
+++ analysis.R
@@ -1,18 +1,31 @@
-library("bayesplot")
 library("rstan")
+library("bayesplot")
+
+
+fit <- readRDS("out/test3_sd_0_51/model_prior_sd_0_2.rds")
+sim_params <- readRDS("out/test3_sd_0_51/params.rds")
+
+summary_list <- summary(fit)
+summary_all_chains <- summary_list$summary
+posterior <- as.matrix(fit)
 
 
 #R-hat
-rhat_mu_S_log_k <- summary_list$summary['mu_s_log_k', 'Rhat']
+rhat_mu_s_log_k <- summary_list$summary['mu_s_log_k', 'Rhat']
 traceplot(fit, pars = 'mu_s_log_k')
 
 
-# Plot
-posterior <- as.matrix(fit)
-mcmc_areas(posterior, pars = c('s_log_k[1]', 's_log_k[2]', 's_log_k[3]'), prob = 0.95)
-mcmc_areas(posterior, pars = c('mu_log_k'), prob = 0.95)
+# dBF
+integrate(posterior, lower = -Inf, upper = 0)
+
+# Savage-Dickey BF
+
+
+# P(effect > 0)
+
+
+# HDI
 mcmc_areas(posterior, pars = c('mu_s_log_k'), prob = 0.95)
-mcmc_areas(posterior, pars = c('mu_beta'), prob = 0.95)
 
 
 
