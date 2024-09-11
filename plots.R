5f94853b8664e400a191cda5ea2ecd5aa51f69e1
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-09-11 18:17:13 +0200

    remove multiplot function

--- plots.R
+++ plots.R
@@ -22,41 +22,104 @@ data_hdi_rope <- aggregate(prop_hdi_in_rope_conv ~ s_log_k_sd + prior_sd, df_res
 
 dodge_width <- 0.7
 
-values_plot <- function(y_column, x_lab, y_lab, hline_intercept, line_data, x_tick_label_size){
-  plot <- ggplot(df_results, aes(x = as.factor(prior_sd), y = !!sym(y_column), color = as.factor(s_log_k_sd))) +
-    labs(x = x_lab, y = y_lab) +
-    geom_jitter(
-      position = position_dodge(width = dodge_width),
-      alpha = 0.5
-    ) +
-    geom_line(data = line_data,
-              aes(x = as.factor(prior_sd), 
-                  y = !!sym(y_column), 
-                  color = as.factor(s_log_k_sd), 
-                  group = as.factor(s_log_k_sd)),
-              position = position_dodge(width = dodge_width)) +
-    geom_hline(yintercept = hline_intercept[1], linetype = 'dashed') +
-    geom_hline(yintercept = hline_intercept[2], linetype = 'dashed') +
-    theme(
-      panel.background = element_blank(),
-      panel.border = element_rect(color = 'black', fill = NA),
-      axis.ticks.length = unit(-0.1, 'cm'),
-      legend.position = 'none',
-      axis.title.y = element_text(size = 9),
-      axis.text.y = element_text(size = 7),
-      axis.text.x = element_text(size = x_tick_label_size)
-    )
-  
-  return(plot)
-}
+plot_savage_dickey_bf <- ggplot(df_results, aes(x = as.factor(prior_sd), y = savage_dickey_bf, color = as.factor(s_log_k_sd))) +
+  labs(x = "", y = expression("Savage-Dickey BF "[10])) +
+  geom_jitter(
+    position = position_dodge(width = dodge_width),
+    alpha = 0.5
+  ) +
+  geom_line(data = data_savage_dickey_bf,
+            aes(x = as.factor(prior_sd), 
+                y = savage_dickey_bf, 
+                color = as.factor(s_log_k_sd), 
+                group = as.factor(s_log_k_sd)),
+            position = position_dodge(width = dodge_width)) +
+  geom_hline(yintercept = 3, linetype = 'dashed') +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(color = 'black', fill = NA),
+    axis.ticks.length = unit(-0.1, 'cm'),
+    legend.position = 'none',
+    axis.title.y = element_text(size = 9),
+    axis.text.y = element_text(size = 7),
+    axis.text.x = element_text(size = 7)
+  )
+
+plot_dbf <- ggplot(df_results, aes(x = as.factor(prior_sd), y = directional_bf, color = as.factor(s_log_k_sd))) +
+  labs(x = "", y = expression("dBF"["+-"])) +
+  scale_y_log10(labels = scales::comma) +
+  geom_jitter(
+    position = position_dodge(width = dodge_width),
+    alpha = 0.5
+  ) +
+  geom_line(data = data_directional_bf,
+            aes(x = as.factor(prior_sd), 
+                y = directional_bf, 
+                color = as.factor(s_log_k_sd), 
+                group = as.factor(s_log_k_sd)),
+            position = position_dodge(width = dodge_width)) +
+  geom_hline(yintercept = 3, linetype = 'dashed') +
+  geom_hline(yintercept = 0.33, linetype = 'dashed') +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(color = 'black', fill = NA),
+    axis.ticks.length = unit(-0.1, 'cm'),
+    legend.position = 'none',
+    axis.title.y = element_text(size = 9),
+    axis.text.y = element_text(size = 7),
+    axis.text.x = element_blank()
+  )
+
+plot_hdi_rope <- ggplot(df_results, aes(x = as.factor(prior_sd), y = prop_hdi_in_rope_conv, color = as.factor(s_log_k_sd))) +
+  labs(x = "Prior SD", y = "Prop. 95% HDI in ROPE") +
+  geom_jitter(
+    position = position_dodge(width = dodge_width),
+    alpha = 0.5
+  ) +
+  geom_line(data = data_hdi_rope,
+            aes(x = as.factor(prior_sd), 
+                y = prop_hdi_in_rope_conv, 
+                color = as.factor(s_log_k_sd), 
+                group = as.factor(s_log_k_sd)),
+            position = position_dodge(width = dodge_width)) +
+  geom_hline(yintercept = 0, linetype = 'dashed') +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(color = 'black', fill = NA),
+    axis.ticks.length = unit(-0.1, 'cm'),
+    legend.position = 'none',
+    axis.title.y = element_text(size = 9),
+    axis.text.y = element_text(size = 7),
+    axis.text.x = element_text(size = 7)
+  )
+
+plot_p_effect <- ggplot(df_results, aes(x = as.factor(prior_sd), y = p_effect, color = as.factor(s_log_k_sd))) +
+  labs(x = "Prior SD", y = "P(effect > 0)") +
+  geom_jitter(
+    position = position_dodge(width = dodge_width),
+    alpha = 0.5
+  ) +
+  geom_line(data = data_p_effect,
+            aes(x = as.factor(prior_sd), 
+                y = p_effect, 
+                color = as.factor(s_log_k_sd), 
+                group = as.factor(s_log_k_sd)),
+            position = position_dodge(width = dodge_width)) +
+  geom_hline(yintercept = 0.95, linetype = 'dashed') +
+  geom_hline(yintercept = 0.05, linetype = 'dashed') +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(color = 'black', fill = NA),
+    axis.ticks.length = unit(-0.1, 'cm'),
+    legend.position = 'none',
+    axis.title.y = element_text(size = 9),
+    axis.text.y = element_text(size = 7),
+    axis.text.x = element_text(size = 7)
+  )
 
 
 # add legend!!!
 
-plot_savage_dickey_bf <- values_plot("savage_dickey_bf", "", expression("Savage-Dickey BF "[10]), c(3,3), data_savage_dickey_bf, 0)
-plot_dbf <- values_plot("directional_bf", "", expression("dBF"["+-"]), c(0.33, 3), data_directional_bf, 0)
-plot_hdi_rope <- values_plot("prop_hdi_in_rope_conv", "Prior SD", "Prop. 95% HDI in ROPE", c(0,0), data_hdi_rope, 7)
-plot_p_effect <- values_plot("p_effect", "Prior SD", "P(effect > 0)", c(0.05, 0.95), data_p_effect, 7)
 
 multiplot_values <- (plot_savage_dickey_bf + plot_dbf) / (plot_hdi_rope + plot_p_effect) +
   plot_annotation(tag_levels = 'A') & theme(plot.tag = element_text(size = 14))
@@ -102,11 +165,11 @@ plot_fp_dbf <- ggplot(data_fp_directional_bf_conv, aes(x = as.factor(prior_sd), 
     legend.title = element_text(size = 7, margin = margin(b = 0)),
     legend.text = element_text(size = 7),
     axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank(),
-  ) +
-  coord_cartesian(ylim = c(0, 0.13))
+    axis.text.x = element_blank()#,
+    #axis.title.y = element_blank(),
+    #axis.text.y = element_blank(),
+  ) #+
+#coord_cartesian(ylim = c(0, 0.13))
 
 plot_fp_p_effect <- ggplot(data_fp_p_effect_conv, aes(x = as.factor(prior_sd), y = fp_p_effect_conv/100, group = as.factor(s_log_k_sd), color = as.factor(s_log_k_sd))) +
   labs(x = "Prior SD", y = "Prop. false positives") +
