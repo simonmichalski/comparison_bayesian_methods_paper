253c0f226c35b4109a36ad51b9bf6251233390f1
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-09-27 15:52:28 +0200

    add more HDIs, P(effect), dBFs

--- analysis.R
+++ analysis.R
@@ -25,7 +25,7 @@ get_savage_dickey_bf <- function(posterior_samples, prior_sd){
 
 
 get_results_df <- function(){
-  results <- vector('list', nrow(df_results))
+  results <- vector('list', length(s_log_k_sds)*num_samples*length(prior_sds))
   counter <- 1
   
   for (i in 1:length(s_log_k_sds)){
@@ -46,27 +46,61 @@ get_results_df <- function(){
         
         p_effect <- sum(posterior_samples_mu_s_log_k > 0) / length(posterior_samples_mu_s_log_k)
         
-        hdi_lower <- hdi(posterior_samples_mu_s_log_k, ci = 0.95)$CI_low
-        hdi_upper <- hdi(posterior_samples_mu_s_log_k, ci = 0.95)$CI_high
+        hdi_80_lower <- hdi(posterior_samples_mu_s_log_k, ci = 0.8)$CI_low
+        hdi_80_upper <- hdi(posterior_samples_mu_s_log_k, ci = 0.8)$CI_high
         
-        results[[counter]] <- c(s_log_k_sds[i], j, prior_sds[k], directional_bf, savage_dickey_bf, p_effect, hdi_lower, hdi_upper)
+        hdi_90_lower <- hdi(posterior_samples_mu_s_log_k, ci = 0.9)$CI_low
+        hdi_90_upper <- hdi(posterior_samples_mu_s_log_k, ci = 0.9)$CI_high
+        
+        hdi_95_lower <- hdi(posterior_samples_mu_s_log_k, ci = 0.95)$CI_low
+        hdi_95_upper <- hdi(posterior_samples_mu_s_log_k, ci = 0.95)$CI_high
+        
+        hdi_99_lower <- hdi(posterior_samples_mu_s_log_k, ci = 0.99)$CI_low
+        hdi_99_upper <- hdi(posterior_samples_mu_s_log_k, ci = 0.99)$CI_high
+        
+        results[[counter]] <- c(s_log_k_sds[i], j, prior_sds[k], directional_bf, savage_dickey_bf, 
+                                p_effect, hdi_80_lower, hdi_80_upper, hdi_90_lower, hdi_90_upper, 
+                                hdi_95_lower, hdi_95_upper, hdi_99_lower, hdi_99_upper)
+        
+        print(paste0(counter, "/", length(s_log_k_sds)*num_samples*length(prior_sds)))
         counter <- counter + 1
       }
     }
   }
   
   results <- as.data.frame(do.call(rbind, results))
-  colnames(results) <- c("s_log_k_sd", "sample", "prior_sd", "directional_bf", "savage_dickey_bf", "p_effect", "hdi_lower", "hdi_upper")
+  colnames(results) <- c("s_log_k_sd", "sample", "prior_sd", "directional_bf", "savage_dickey_bf", 
+                         "p_effect", "hdi_80_lower", "hdi_80_upper", "hdi_90_lower", "hdi_90_upper", 
+                         "hdi_95_lower", "hdi_95_upper", "hdi_99_lower", "hdi_99_upper")
   
-  # Add false positive (fp) result columns with conventional decision thresholds (conv)
+  # Add false positive (fp) result columns with conventional decision thresholds
   # Distinction between positive (pos) and negative (neg) false positive effects
-  results$fp_savage_dickey_bf_conv <- ifelse(results$savage_dickey_bf > 3, 1, 0)
-  results$fp_directional_bf_pos_conv <- ifelse(results$directional_bf > 3, 1, 0)
-  results$fp_directional_bf_neg_conv <- ifelse(results$directional_bf < 0.33, 1, 0)
-  results$fp_p_effect_pos_conv <- ifelse(results$p_effect > 0.975, 1, 0)
-  results$fp_p_effect_neg_conv <- ifelse(results$p_effect < 0.025, 1, 0)
-  results$fp_hdi_pos_conv <- ifelse(results$hdi_lower > 0, 1, 0)
-  results$fp_hdi_neg_conv <- ifelse(results$hdi_upper < 0, 1, 0)
+  results$fp_savage_dickey_bf_3 <- ifelse(results$savage_dickey_bf > 3, 1, 0)
+  
+  results$fp_directional_bf_3_pos <- ifelse(results$directional_bf > 3, 1, 0)
+  results$fp_directional_bf_1_3_neg <- ifelse(results$directional_bf < 1/3, 1, 0)
+  results$fp_directional_bf_10_pos <- ifelse(results$directional_bf > 10, 1, 0)
+  results$fp_directional_bf_1_10_neg <- ifelse(results$directional_bf < 1/10, 1, 0)
+  results$fp_directional_bf_30_pos <- ifelse(results$directional_bf > 30, 1, 0)
+  results$fp_directional_bf_1_30_neg <- ifelse(results$directional_bf < 1/30, 1, 0)
+  results$fp_directional_bf_100_pos <- ifelse(results$directional_bf > 100, 1, 0)
+  results$fp_directional_bf_1_100_neg <- ifelse(results$directional_bf < 1/100, 1, 0)
+  
+  results$fp_p_effect_95_pos <- ifelse(results$p_effect > 0.95, 1, 0)
+  results$fp_p_effect_05_neg <- ifelse(results$p_effect < 0.05, 1, 0)
+  results$fp_p_effect_975_pos <- ifelse(results$p_effect > 0.975, 1, 0)
+  results$fp_p_effect_025_neg <- ifelse(results$p_effect < 0.025, 1, 0)
+  results$fp_p_effect_99_pos <- ifelse(results$p_effect > 0.99, 1, 0)
+  results$fp_p_effect_01_neg <- ifelse(results$p_effect < 0.01, 1, 0)
+  
+  results$fp_hdi_80_pos <- ifelse(results$hdi_80_lower > 0, 1, 0)
+  results$fp_hdi_80_neg <- ifelse(results$hdi_80_upper < 0, 1, 0)
+  results$fp_hdi_90_pos <- ifelse(results$hdi_90_lower > 0, 1, 0)
+  results$fp_hdi_90_neg <- ifelse(results$hdi_90_upper < 0, 1, 0)
+  results$fp_hdi_95_pos <- ifelse(results$hdi_95_lower > 0, 1, 0)
+  results$fp_hdi_95_neg <- ifelse(results$hdi_95_upper < 0, 1, 0)
+  results$fp_hdi_99_pos <- ifelse(results$hdi_99_lower > 0, 1, 0)
+  results$fp_hdi_99_neg <- ifelse(results$hdi_99_upper < 0, 1, 0)
   
   return(results)
 }
