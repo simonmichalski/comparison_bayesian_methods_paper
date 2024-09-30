8c5e8ed5715b40a29d0332efaa80bcbe0db035f8
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-09-30 11:53:19 +0200

    change variable name and decision rule

--- convergence_check.R
+++ convergence_check.R
@@ -1,6 +1,6 @@
 library("rstan")
 
-s_log_k_sds <- c(0.2)
+s_log_k_sds <- c(0.2, 0.51, 0.81)
 num_samples <- 100
 prior_sds <- c(0.05, 0.1, 0.2, 0.5, 1, 1.5, 2, 2.5)
 
@@ -16,10 +16,14 @@ convergence_check <- function(){
         fit_path <- file.path(sample_path, paste0("model_prior_sd_", gsub("\\.", "_", prior_sds[k]), ".rds"))
         fit <- readRDS(fit_path)
         
-        rhat_mu_s_log_k <- summary(fit)$summary[,'Rhat']
+        rhats <- summary(fit)$summary[,'Rhat']
         
-        if (any(round(rhat_mu_s_log_k, 2) < 1) || any(round(rhat_mu_s_log_k, 2) > 1.1)){
+        if (any(rhats > 1.1)){
           print(paste("Model not converged:", fit_path))
+        }
+        
+        if (any(round(rhat_mu_s_log_k, 2) < 1)){
+          print(paste("Rhat < 1", fit_path))
         }
       }
     }
