6bd019c1ec56d4af02fdc47635d4ab1e3918a8d8
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-10-09 09:56:06 +0200

    minor changes

--- convergence_check.R
+++ convergence_check.R
@@ -1,11 +1,15 @@
 library("rstan")
 
 s_log_k_sds <- c(0.2, 0.51, 0.81)
-num_samples <- 100
+num_samples <- 200
 prior_sds <- c(0.05, 0.1, 0.2, 0.5, 1, 1.5, 2, 2.5)
 
 
 convergence_check <- function(){
+  num_models <- length(s_log_k_sds)*num_samples*length(prior_sds)
+  not_converged <- list()
+  counter <- 1
+  
   for (i in 1:length(s_log_k_sds)){
     sd_path <- file.path("out", paste0("sd_", gsub("\\.", "_", s_log_k_sds[i])))
     
@@ -18,17 +22,17 @@ convergence_check <- function(){
         
         rhats <- summary(fit)$summary[,'Rhat']
         
-        if (any(rhats > 1.1)){
-          print(paste("Model not converged:", fit_path))
+        if (any(rhats > 1.01)){
+          not_converged <- append(not_converged, fit_path)
         }
         
-        if (any(round(rhat_mu_s_log_k, 2) < 1)){
-          print(paste("Rhat < 1", fit_path))
-        }
+        print(paste0(counter, "/", num_models))
+        counter <- counter + 1
       }
     }
   }
+  return(not_converged)
 }
 
 
-convergence_check()
+not_converged_list <- convergence_check()
