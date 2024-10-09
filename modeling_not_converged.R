5bc22817396d50cca70c8d38d3a365d04aba0f51
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-10-09 09:57:30 +0200

    add file

--- modeling_not_converged.R
+++ modeling_not_converged.R
@@ -3,12 +3,14 @@ library('doParallel')
 library('parallel')
 library('rstan')
 
-not_converged <- readRDS("not_converged.rds")
+# Read .rds as character vector
+not_converged <- unlist(readRDS("not_converged.rds"))
+
 
 get_vectors <- function(){
-  sds <- vector()
-  sample_numbers <- vector()
-  prior_sds <- vector()
+  sds <- c()
+  sample_numbers <- c()
+  prior_sds <- c()
   
   for (path in not_converged){
     sd_match <- regmatches(path, regexpr("sd_\\d+_\\d+", path))
@@ -28,11 +30,12 @@ get_vectors <- function(){
   return(list(sds = sds, sample_numbers = sample_numbers, prior_sds = prior_sds))
 }
 
+
 vectors <- get_vectors()
 sds <- vectors$sds
 sample_numbers <- vectors$sample_numbers
 prior_sds <- vectors$prior_sds
-
+remove(vectors)
 
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
@@ -41,8 +44,8 @@ rstan_options(auto_write = TRUE)
 get_stan_data <- function(){
   stan_data_list <- list()
 
-  for (i in not_converged){
-    data <- readRDS(i)
+  for (i in 1:length(not_converged)){
+    data <- readRDS(file.path("out", paste0("sd_", gsub("0.", "0_", sds[i])), paste0("sample_", sample_numbers[i]), "data.rds"))
 
     # Create 3d arrays; condition index 1:2
     condition_values <- data$condition
@@ -69,10 +72,10 @@ get_stan_data <- function(){
   return(stan_data_list)
 }
 
+stan_data_list <- get_stan_data()
+
 
 parallel_modelling <- function(){
-  stan_data_list <- get_stan_data()
-
   num_cores <- detectCores(logical = TRUE)
   cluster <- makeCluster(num_cores - 1)
   registerDoParallel(cluster)
@@ -85,14 +88,20 @@ parallel_modelling <- function(){
       model,
       data = stan_data_list[[i]],
       chains = 2,
-      iter = 2000,
+      iter = 3000,
       warmup = 2000,
       thin = 1,
       save_warmup = FALSE
     )
 
-    sd_path <- file.path("out", paste0("sd_", gsub("0.", "0_", sds[i])))
-    model_path <- file.path(sd_path, paste0("sample_", sample_numbers[i]), paste0("model_prior_sd_", gsub("\\.", "_", prior_sds[i]), ".rds"))
+    model_path <- file.path("out_refit", paste0("sd_", gsub("0.", "0_", sds[i])), paste0("sample_", sample_numbers[i]))
+    
+    if (!dir.exists(model_path)) {
+      dir.create(model_path, recursive = TRUE)
+    }
+    
+    model_path <- file.path(model_path, paste0("model_prior_sd_", gsub("\\.", "_", prior_sds[i]), ".rds"))
+    
     saveRDS(fit, model_path)
   }
 
@@ -100,4 +109,69 @@ parallel_modelling <- function(){
 }
 
 
+fit_till_conversion_loop <- function(){
+  
+  # Convergence check after first refit
+  still_not_converged <- c()
+  
+  for (i in 1:length(not_converged)){
+    refit_path <- gsub("out", "out_refit", not_converged[i])
+    fit <- readRDS(refit_path)
+    
+    rhats <- summary(fit)$summary[,'Rhat']
+    
+    if (any(rhats > 1.01)){
+      still_not_converged <- append(still_not_converged, i)
+    }
+  }
+  
+  while(length(still_not_converged) > 0){
+    # Fit still not converged models
+    stan_data_list_not_converged <- stan_data_list[still_not_converged]
+    sds_not_converged <- sds[still_not_converged]
+    sample_numbers_not_converged <- sample_numbers[still_not_converged]
+    prior_sds_not_converged <- prior_sds[still_not_converged]
+    
+    num_cores <- detectCores(logical = TRUE)
+    cluster <- makeCluster(num_cores - 1)
+    registerDoParallel(cluster)
+    
+    foreach (i = 1:length(stan_data_list_not_converged), .packages = 'rstan', .export=ls(envir=globalenv())) %dopar% {
+      
+      model <- stan_model(file.path("models", paste0("discounting_model_prior_sd_", gsub("\\.", "_", prior_sds[i]), ".stan")))
+      
+      fit <- sampling(
+        model,
+        data = stan_data_list_not_converged[[i]],
+        chains = 2,
+        iter = 3000,
+        warmup = 2000,
+        thin = 1,
+        save_warmup = FALSE
+      )
+      
+      model_path <- file.path("out_refit", paste0("sd_", gsub("0.", "0_", sds[i])), paste0("sample_", sample_numbers[i]), paste0("model_prior_sd_", gsub("\\.", "_", prior_sds[i]), ".rds"))
+      saveRDS(fit, model_path)
+    }
+    
+    stopCluster(cl = cluster)
+    
+    # Convergence check
+    still_not_converged <- c()
+    
+    for (i in 1:length(not_converged)){
+      refit_path <- gsub("out", "out_refit", not_converged[i])
+      fit <- readRDS(refit_path)
+      
+      rhats <- summary(fit)$summary[,'Rhat']
+      
+      if (any(rhats > 1.01)){
+        still_not_converged <- append(still_not_converged, i)
+      }
+    }
+  }
+}
+
+
 parallel_modelling()
+#fit_till_conversion_loop()
