c9f334d308e0008be670cd131fb1de11b300e796
Simon Michalski <smichal2@smail.uni-koeln.de>
2024-10-09 16:23:05 +0200

    working version all in one file

--- analysis.R
+++ analysis.R
@@ -5,6 +5,11 @@ s_log_k_sds <- c(0.2, 0.51, 0.81)
 num_samples <- 200
 prior_sds <- c(0.05, 0.1, 0.2, 0.5, 1, 1.5, 2, 2.5)
 
+n_models <- length(s_log_k_sds)*num_samples*length(prior_sds)
+
+hdi_seq <- seq(from = 0.9, to = 1, by = 0.0001)
+n_tests <- 10
+
 
 # dBF+-
 get_directional_bf <- function(posterior_samples, prior_sd){
@@ -24,9 +29,8 @@ get_savage_dickey_bf <- function(posterior_samples, prior_sd){
 }
 
 
-get_results_df <- function(){
-  start_time <- Sys.time()
-  
+# Compute inference methods for every model (Savage-Dickey BF, dBF, P(effect > 0), 80% HDI, 90% HDI, 95% HDI, 99% HDI)
+get_results <- function(){
   results <- vector('list', length(s_log_k_sds)*num_samples*length(prior_sds))
   counter <- 1
   
@@ -41,6 +45,7 @@ get_results_df <- function(){
         fit <- readRDS(fit_path)
         
         posterior_samples_mu_s_log_k <- extract(fit)$mu_s_log_k
+        remove(fit)
         
         directional_bf <- get_directional_bf(posterior_samples_mu_s_log_k, prior_sds[k])
         
@@ -69,14 +74,105 @@ get_results_df <- function(){
       }
     }
   }
-  
   results <- as.data.frame(do.call(rbind, results))
   colnames(results) <- c("s_log_k_sd", "sample", "prior_sd", "directional_bf", "savage_dickey_bf", 
                          "p_effect", "hdi_80_lower", "hdi_80_upper", "hdi_90_lower", "hdi_90_upper", 
                          "hdi_95_lower", "hdi_95_upper", "hdi_99_lower", "hdi_99_upper")
+  return(results)
+}
+
+
+# Compute upper and lower bounds of all HDIs in hdi_seq for every model
+get_hdi_bounds <- function(){
+  model_hdis <- vector('list', n_models)
+  model_counter <- 1
   
-  # Add false positive (fp) result columns with conventional decision thresholds
-  # Distinction between positive (pos) and negative (neg) false positive effects
+  for (i in 1:length(s_log_k_sds)){
+    
+    for (j in 1:num_samples){
+      
+      for (k in 1:length(prior_sds)){
+        fit_path <- file.path("out", paste0("sd_", gsub("\\.", "_", s_log_k_sds[i])), paste0("sample_", j), paste0("model_prior_sd_", gsub("\\.", "_", prior_sds[k]), ".rds"))
+        fit <- readRDS(fit_path)
+        
+        posterior_samples_mu_s_log_k <- extract(fit)$mu_s_log_k
+        remove(fit)
+        
+        hdis <- vector('list', length(hdi_seq))
+        hdi_counter <- 1
+        for (hdi_width in hdi_seq){
+          hdi_lower <- hdi(posterior_samples_mu_s_log_k, ci = hdi_width)$CI_low
+          hdi_upper <- hdi(posterior_samples_mu_s_log_k, ci = hdi_width)$CI_high
+          
+          hdis[[hdi_counter]] <- c(hdi_lower, hdi_upper)
+          hdi_counter <- hdi_counter + 1
+        }
+        
+        model_hdis[[model_counter]] <- hdis
+        print(paste0(model_counter, "/", n_models))
+        model_counter <- model_counter + 1
+      }
+    }
+  }
+  return(model_hdis)
+}
+
+
+# Compute simulation-based decision thresholds
+get_simulation_based_thresholds <- function(results, model_hdis){
+  sim_based_thresholds <- data.frame()
+  
+  for (i in 1:n_tests){
+    savage_dickey_bf <- quantile(results$savage_dickey_bf, probs = 1-(0.05/i))[[1]]
+    
+    directional_bf_upper <- quantile(results$directional_bf, probs = 1-(0.025/i))[[1]]
+    directional_bf_lower <- quantile(results$directional_bf, probs = 0.025/i)[[1]]
+    
+    p_effect_upper <- quantile(results$p_effect, probs = 1-(0.025/i))[[1]]
+    p_effect_lower <- quantile(results$p_effect, probs = 0.025/i)[[1]]
+    
+    sim_based_thresholds <- rbind(sim_based_thresholds, c(i, savage_dickey_bf, directional_bf_upper, directional_bf_lower, p_effect_upper, p_effect_lower))
+  }
+  colnames(sim_based_thresholds) <- c("n_tests", "savage_dickey_bf", "directional_bf_upper", "directional_bf_lower", "p_effect_upper", "p_effect_lower")
+  
+  sim_based_thresholds$hdi <- get_sim_based_hdi_widths(model_hdis)
+  
+  return(sim_based_thresholds)
+}
+
+
+get_sim_based_hdi_widths <- function(model_hdis){
+  proportions_false_positives <- c()
+  
+  for (hdi in 1:length(hdi_seq)){
+    num_false_positives <- 0
+    
+    for (model in 1:n_models){
+      hdi_lower <- model_hdis[[model]][[hdi]][1]
+      hdi_upper <- model_hdis[[model]][[hdi]][2]
+      
+      num_false_positives <- num_false_positives + ifelse(hdi_upper < 0 | hdi_lower > 0, 1, 0)
+    }
+    proportions_false_positives <- append(proportions_false_positives, num_false_positives/n_models)
+  }
+  
+  sim_based_hdis <- c()
+  
+  for (tests in 1:n_tests){
+    # Get indices of relevant false positive proportions
+    indices <- which(proportions_false_positives <= (0.05/tests))
+    # Get index of closest false positive proportion
+    hdi_seq_index <- indices[which.min(abs(proportions_false_positives[indices] - (0.05/tests)))]
+    sim_based_hdis <- append(sim_based_hdis, hdi_seq[hdi_seq_index])
+  }
+  
+  return(sim_based_hdis)
+}
+
+
+# Add false positive (fp) result columns with conventional and simulation-based (sim) decision thresholds
+# Distinction between positive (pos) and negative (neg) false positive effects
+add_false_positive_results_columns <- function(results, model_hdis, sim_thres){
   results$fp_savage_dickey_bf_3 <- ifelse(results$savage_dickey_bf > 3, 1, 0)
   
   results$fp_directional_bf_3_pos <- ifelse(results$directional_bf > 3, 1, 0)
@@ -104,12 +200,36 @@ get_results_df <- function(){
   results$fp_hdi_99_pos <- ifelse(results$hdi_99_lower > 0, 1, 0)
   results$fp_hdi_99_neg <- ifelse(results$hdi_99_upper < 0, 1, 0)
   
-  end_time <- Sys.time() - start_time
-  cat(end_time, attr(end_time, "units"))
+  results$fp_savage_dickey_bf_sim <- ifelse(results$savage_dickey_bf > sim_thres$savage_dickey_bf[1], 1, 0)
+  results$fp_directional_bf_sim_pos <- ifelse(results$directional_bf > sim_thres$directional_bf_upper[1], 1, 0)
+  results$fp_directional_bf_sim_neg <- ifelse(results$directional_bf < sim_thres$directional_bf_lower[1], 1, 0)
+  results$fp_p_effect_sim_pos <- ifelse(results$p_effect > sim_thres$p_effect_upper[1], 1, 0)
+  results$fp_p_effect_sim_neg <- ifelse(results$p_effect < sim_thres$p_effect_lower[1], 1, 0)
+  
+  fp_hdi_sim_pos <- c()
+  fp_hdi_sim_neg <- c()
+  
+  for (i in 1:n_models){
+    seq_index <- which(hdi_seq == sim_thres$hdi[1])
+    lower_bounds <- model_hdis[[i]][[seq_index]][1]
+    upper_bounds <- model_hdis[[i]][[seq_index]][2]
+    fp_hdi_sim_pos <- append(fp_hdi_sim_pos, ifelse(lower_bounds > 0, 1, 0))
+    fp_hdi_sim_neg <- append(fp_hdi_sim_neg, ifelse(upper_bounds < 0, 1, 0))
+  }
+  
+  results$fp_hdi_sim_pos <- fp_hdi_sim_pos
+  results$fp_hdi_sim_neg <- fp_hdi_sim_neg
   
   return(results)
 }
 
 
-df_results <- get_results_df()
-saveRDS(df_results, "final_results.rds")
+results <- get_results()
+hdi_bounds <- get_hdi_bounds()
+sim_based_thresholds <- get_simulation_based_thresholds(results, hdi_bounds)
+final_results <- add_false_positive_results_columns(results, hdi_bounds, sim_based_thresholds)
+
+saveRDS(results, "results.rds")
+saveRDS(hdi_bounds, "hdi_bounds.rds")
+saveRDS(sim_based_thresholds, "sim_based_thresholds.rds")
+saveRDS(final_results, "final_results.rds")
